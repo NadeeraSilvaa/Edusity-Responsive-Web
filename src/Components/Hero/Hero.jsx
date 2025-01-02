245e715955d5239af80e5208ad3b846b5d62f957
Nadeera nirmal silva <nadeera.silva01@gmail.com>
2025-01-02 20:19:54 +0530

    Added Header and Hero Section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-text">
+            <h1>We ensure better education for a better world</h1>
+            <p>Our cutting-edge curriculam is designed to empower students with the knowledge, skills, and experiences needed to excel in the dynamic field of education.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
